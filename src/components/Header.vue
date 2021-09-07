532edc5d99f9c7652cd464c308270819892d0aca
bePIRRO <manuel.pirro@gmail.com>
2021-09-07 17:08:42 +0200

    del helloworld.vue + start header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="navbar">
+    <div class="row">
+      <div class="col">
+        <a href="#"><img src="@/img/dc-logo.png" alt=""/></a>
+      </div>
+      <div class="col">
+        <ul>
+          <!-- <li v-for="link in links"></li> -->
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style>
+.navbar {
+  background-color: #fff;
+  height: 100px;
+  display: flex;
+}
+
+.row {
+  justify-content: space-between;
+}
+</style>
+
+<script></script>
