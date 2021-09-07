1d9c7eb1a73eeb36c28de8277d281d2c0b328028
bePIRRO <manuel.pirro@gmail.com>
2021-09-07 17:44:50 +0200

    header navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,11 +2,15 @@
   <header class="navbar">
     <div class="row">
       <div class="col">
-        <a href="#"><img src="@/img/dc-logo.png" alt=""/></a>
+        <a href="#" id="logo"><img src="@/img/dc-logo.png" alt="DC Comics"/></a>
       </div>
       <div class="col">
         <ul>
-          <!-- <li v-for="link in links"></li> -->
+          <li v-for="(link, index) in links" :key="index">
+            <a :href="link.url" :class="{ active: link.current }">{{
+              link.text
+            }}</a>
+          </li>
         </ul>
       </div>
     </div>
@@ -17,12 +21,96 @@
 .navbar {
   background-color: #fff;
   height: 100px;
-  display: flex;
 }
 
 .row {
-  justify-content: space-between;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: auto;
+}
+
+.active,
+a:hover:not(#logo) {
+  border-bottom: 2px solid blue;
+}
+
+.row div {
+  display: inline-block;
+  height: auto;
+}
+
+li {
+  list-style-type: none;
+  display: inline;
+  height: auto;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+  margin: 0 10px;
 }
 </style>
 
-<script></script>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
