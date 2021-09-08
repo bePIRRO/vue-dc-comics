06bbb3f3cb8250797924ac8d0a11fd1ba145ab83
bePIRRO <manuel.pirro@gmail.com>
2021-09-08 16:19:19 +0200

    header + hero

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,47 @@
 <template>
-  <header class="navbar">
-    <div class="row">
-      <div class="col">
-        <a href="#" id="logo"><img src="@/img/dc-logo.png" alt="DC Comics"/></a>
-      </div>
-      <div class="col">
-        <ul>
-          <li v-for="(link, index) in links" :key="index">
-            <a :href="link.url" :class="{ active: link.current }">{{
-              link.text
-            }}</a>
-          </li>
-        </ul>
+  <header>
+    <!-- navbar -->
+    <div class="navbar">
+      <div class="row">
+        <div class="col">
+          <a href="#" id="logo"
+            ><img src="@/img/dc-logo.png" alt="DC Comics"
+          /></a>
+        </div>
+        <div class="col">
+          <ul>
+            <li v-for="(link, index) in links" :key="index">
+              <a :href="link.url" :class="{ active: link.current }">{{
+                link.text
+              }}</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
+
+    <!-- hero -->
+
+    <div id="hero"></div>
   </header>
 </template>
 
 <style>
 .navbar {
   background-color: #fff;
-  height: 100px;
 }
 
 .row {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  height: auto;
+  height: 100px;
 }
 
 .active,
 a:hover:not(#logo) {
-  border-bottom: 2px solid blue;
+  color: #0282f9;
+  border-bottom: 2px solid #0282f9;
 }
 
 .row div {
@@ -40,16 +49,34 @@ a:hover:not(#logo) {
   height: auto;
 }
 
+img {
+  width: auto;
+  max-height: 80px;
+}
+
+.col,
+ul,
+li {
+  height: auto;
+}
+
 li {
   list-style-type: none;
   display: inline;
-  height: auto;
 }
 
 a {
   color: #000;
   text-decoration: none;
   margin: 0 10px;
+}
+
+#hero {
+  width: 100vw;
+  height: 400px;
+  background-image: url("../img/jumbotron.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
 
