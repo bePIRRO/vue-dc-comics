bfb49abc309b8cfd00b6d747f7c9efa932c45547
bePIRRO <manuel.pirro@gmail.com>
2021-09-08 16:57:26 +0200

    start main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -72,7 +72,7 @@ a {
 }
 
 #hero {
-  width: 100vw;
+  width: 100%;
   height: 400px;
   background-image: url("../img/jumbotron.jpg");
   background-repeat: no-repeat;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,33 @@
 <template>
-  <main></main>
+  <main>
+    <!-- current series tag -->
+    <div class="tag">
+      CURRENT SERIES
+    </div>
+
+    <!-- cards container -->
+    <div class="container"></div>
+  </main>
 </template>
 
-<style></style>
+<style>
+main {
+  background-color: #1c1c1c;
+  height: 1000px;
+  margin: 0;
+}
+
+.container {
+  margin: 0;
+}
+
+.tag {
+  background-color: #0282f9;
+  display: inline;
+  color: #fff;
+  padding: 1.05rem;
+  box-shadow: black 5px 2px 2px;
+  position: relative;
+  left: 20%;
+}
+</style>
